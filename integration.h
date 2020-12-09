39d699afc45ef35371201166317b73065bef219e
La <Dperalta@unal.edu.co>
2020-12-09 11:34:25 -0500

    sympletic integrators

--- integration.h
+++ integration.h
@@ -154,4 +154,85 @@ state_t R_L_R(state_t data, state_t & A, double t)
     return A;
 }
 
+template<typename system_t, typename state_t>
+void int_4(system_t deriv  ,state_t & data, double t, double h){
+
+state_t dydt(data.size());
+state_t map(8);
+
+double K = std::cbrt(2.0);
+
+map[0] = 1.0/(2.0*(2.0-K));//c_1
+map[2] = (1.0-K)/(2.0*(2.0-K));//c_2
+map[4] = map[2];//c_3
+map[6] = map[0];//c_4
+
+map[1] = 1.0/(2.0-K);//d_1
+map[3] = -K/(2.0-K);//d_2
+map[5] = map[1];//d_3
+map[7] = 0;//d_4
+
+
+for(int kk = 0; kk < 4; ++kk)
+{
+
+  deriv(data, dydt, t);
+
+    for(int ii = 0; ii < 2 ; ++ii){
+      data[ii] = data[ii] + h*map[0 + 2*kk]*dydt[ii];
+    }
+
+  deriv(data, dydt, t);
+
+    for(int jj = 2; jj < 4 ; ++jj){
+     data[jj] = data[jj] + h*map[1 + 2*kk]*dydt[jj];
+    }
+
+  }
+
+}
+
+template<typename system_t, typename state_t>
+void int_6(system_t deriv  ,state_t & data, double t, double h){
+
+state_t dydt(data.size());
+state_t map(16);
+
+
+map[0] = 0.392256805238780;//c_1
+map[2] = 0.510043411918458;//c_2
+map[4] = -0.471053385409758;//c_3
+map[6] = 0.0687531682525198;//c_4
+map[8] = map[6];//c_5
+map[10] = map[4];//c_6
+map[12]= map[2];//c_7
+map[14] = map[0];//c_8
+
+map[1] = 0.784513610477560;//d_1
+map[3] = 0.235573213359357;//d_2
+map[5] = -1.17767998417887;//d_3
+map[7] = 1.31518632068391;//d_4
+map[9] = map[5];//d_5
+map[11] = map[3];//d_6
+map[13] = map[1];//d_7
+map[15] = 0;//d_8
+
+for(int kk = 0; kk < 8; ++kk)
+{
+
+  deriv(data, dydt, t);
+
+    for(int ii = 0; ii < 2 ; ++ii){
+      data[ii] = data[ii] + h*map[2*kk]*dydt[ii];
+    }
+
+  deriv(data, dydt, t);
+
+    for(int jj = 2; jj < 4 ; ++jj){
+     data[jj] = data[jj] + h*map[1 + 2*kk]*dydt[jj];
+    }
+
+  }
+
+}
 #endif // __INTEGRATION_H_
